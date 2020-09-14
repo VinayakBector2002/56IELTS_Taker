4e991350193ad4a426e5bf20a8e65ed7a760bb91
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2020-09-14 20:12:49 +0530

    Add files via upload

--- Ielts test taker.py
+++ Ielts test taker.py
@@ -21,212 +21,53 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 
 def txt_Editor():
-    import tkinter 
-    import os	 
-    from tkinter.messagebox import *
-    from tkinter.filedialog import *
-
-    class Notepad: 
-
-        __root = Tk() 
-
-        # default window width and height 
-        __thisWidth = 300
-        __thisHeight = 300
-        __thisTextArea = Text(__root) 
-        __thisMenuBar = Menu(__root) 
-        __thisFileMenu = Menu(__thisMenuBar, tearoff=0) 
-        __thisEditMenu = Menu(__thisMenuBar, tearoff=0) 
-        __thisHelpMenu = Menu(__thisMenuBar, tearoff=0) 
-        
-        # To add scrollbar 
-        __thisScrollBar = Scrollbar(__thisTextArea)	 
-        __file = None
-
-        def __init__(self,**kwargs): 
-
-            # Set icon 
-            try: 
-                    self.__root.wm_iconbitmap("Notepad.ico") 
-            except: 
-                    pass
-
-            # Set window size (the default is 300x300) 
-
-            try: 
-                self.__thisWidth = kwargs['width'] 
-            except KeyError: 
-                pass
-
-            try: 
-                self.__thisHeight = kwargs['height'] 
-            except KeyError: 
-                pass
-
-            # Set the window text 
-            self.__root.title("Untitled - Notepad") 
-
-            # Center the window 
-            screenWidth = self.__root.winfo_screenwidth() 
-            screenHeight = self.__root.winfo_screenheight() 
-        
-            # For left-alling 
-            left = (screenWidth / 2) - (self.__thisWidth / 2) 
-            
-            # For right-allign 
-            top = (screenHeight / 2) - (self.__thisHeight /2) 
-            
-            # For top and bottom 
-            self.__root.geometry('%dx%d+%d+%d' % (self.__thisWidth, 
-                                                self.__thisHeight, 
-                                                left, top)) 
-
-            # To make the textarea auto resizable 
-            self.__root.grid_rowconfigure(0, weight=1) 
-            self.__root.grid_columnconfigure(0, weight=1) 
-
-            # Add controls (widget) 
-            self.__thisTextArea.grid(sticky = N + E + S + W) 
-            
-            # To open new file 
-            self.__thisFileMenu.add_command(label="New", 
-                                            command=self.__newFile)	 
-            
-            # To open a already existing file 
-            self.__thisFileMenu.add_command(label="Open", 
-                                            command=self.__openFile) 
-            
-            # To save current file 
-            self.__thisFileMenu.add_command(label="Save", 
-                                            command=self.__saveFile)	 
-
-            # To create a line in the dialog		 
-            self.__thisFileMenu.add_separator()										 
-            self.__thisFileMenu.add_command(label="Exit", 
-                                            command=self.__quitApplication) 
-            self.__thisMenuBar.add_cascade(label="File", 
-                                        menu=self.__thisFileMenu)	 
-            
-            # To give a feature of cut 
-            self.__thisEditMenu.add_command(label="Cut", 
-                                            command=self.__cut)			 
-        
-            # to give a feature of copy	 
-            self.__thisEditMenu.add_command(label="Copy", 
-                                            command=self.__copy)		 
-            
-            # To give a feature of paste 
-            self.__thisEditMenu.add_command(label="Paste", 
-                                            command=self.__paste)		 
-            
-            # To give a feature of editing 
-            self.__thisMenuBar.add_cascade(label="Edit", 
-                                        menu=self.__thisEditMenu)	 
-            
-            # To create a feature of description of the notepad 
-            self.__thisHelpMenu.add_command(label="About Notepad", 
-                                            command=self.__showAbout) 
-            self.__thisMenuBar.add_cascade(label="Help", 
-                                        menu=self.__thisHelpMenu) 
-
-            self.__root.config(menu=self.__thisMenuBar) 
-
-            self.__thisScrollBar.pack(side=RIGHT,fill=Y)					 
-            
-            # Scrollbar will adjust automatically according to the content		 
-            self.__thisScrollBar.config(command=self.__thisTextArea.yview)	 
-            self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set) 
-        
-            
-        def __quitApplication(self): 
-            self.__root.destroy() 
-            # exit() 
-
-        def __showAbout(self): 
-            showinfo("Notepad","Nothing to hide :)") 
-
-        def __openFile(self): 
-            
-            self.__file = askopenfilename(defaultextension=".txt", 
-                                        filetypes=[("All Files","*.*"), 
-                                            ("Text Documents","*.txt")]) 
-
-            if self.__file == "": 
-                
-                # no file to open 
-                self.__file = None
-            else: 
-                
-                # Try to open the file 
-                # set the window title 
-                self.__root.title(os.path.basename(self.__file) + " - Notepad") 
-                self.__thisTextArea.delete(1.0,END) 
-
-                file = open(self.__file,"r") 
-
-                self.__thisTextArea.insert(1.0,file.read()) 
-
-                file.close() 
-
-            
-        def __newFile(self): 
-            self.__root.title("Untitled - Notepad") 
-            self.__file = None
-            self.__thisTextArea.delete(1.0,END) 
-
-        def __saveFile(self): 
-
-            if self.__file == None: 
-                # Save as new file 
-                self.__file = asksaveasfilename(initialfile='Untitled.txt', 
-                                                defaultextension=".txt", 
-                                                filetypes=[("All Files","*.*"), 
-                                                    ("Text Documents","*.txt")]) 
-
-                if self.__file == "": 
-                    self.__file = None
-                else: 
-                    
-                    # Try to save the file 
-                    file = open(self.__file,"w") 
-                    file.write(self.__thisTextArea.get(1.0,END)) 
-                    file.close() 
-                    
-                    # Change the window title 
-                    self.__root.title(os.path.basename(self.__file) + " - Notepad") 
-                    
-                
-            else: 
-                file = open(self.__file,"w") 
-                file.write(self.__thisTextArea.get(1.0,END)) 
-                file.close() 
-
-        def __cut(self): 
-            self.__thisTextArea.event_generate("<<Cut>>") 
-
-        def __copy(self): 
-            self.__thisTextArea.event_generate("<<Copy>>") 
-
-        def __paste(self): 
-            self.__thisTextArea.event_generate("<<Paste>>") 
-
-        def run(self): 
-
-            # Run main application 
-            self.__root.mainloop() 
-
-
-
-
-    # Run main application 
-    notepad = Notepad(width=600,height=400) 
-    notepad.run() 
+    import Notepad
 
 def Delete_screen():
     screen.destroy()
+def Listening_Test():
+    from tkinter import filedialog
+    from pygame import mixer
 
+    class MusicPlayer:
+        def __init__(self, window ):
+            window.geometry('320x100'); window.title('Iris Player'); window.resizable(0,0)
+            Load = Button(window, text = 'Load',  width = 10, font = ('Times', 10), command = self.load)
+            Play = Button(window, text = 'Play',  width = 10,font = ('Times', 10), command = self.play)
+            Pause = Button(window,text = 'Pause',  width = 10, font = ('Times', 10), command = self.pause)
+            Stop = Button(window ,text = 'Stop',  width = 10, font = ('Times', 10), command = self.stop)
+            Load.place(x=0,y=20);Play.place(x=110,y=20);Pause.place(x=220,y=20);Stop.place(x=110,y=60) 
+            self.music_file = False
+            self.playing_state = False
+        def load(self):
+            self.music_file = filedialog.askopenfilename()
+        def play(self):
+            if self.music_file:
+                mixer.init()
+                mixer.music.load(self.music_file)
+                mixer.music.play()
+        def pause(self):
+            if not self.playing_state:
+                mixer.music.pause()
+                self.playing_state=True
+            else:
+                mixer.music.unpause()
+                self.playing_state = False
+        def stop(self):
+            mixer.music.stop()
+    global Listening
+    Listening = Toplevel(screen)
+    Listening.title("IELTS Listening")
+    Listening.geometry("500x500")
+    MusicPlayer(Listening)   
 def speaking_Test():
+    def OG2():
+        Button(speaking,text = "Stop Recording ",height = "2", width = "30", command = stop).pack()
+        import VoiceRecorder
+
+
     def Og():
+        Button(speaking,text = "Stop Recording ",height = "2", width = "30", command = stop).pack()
         import argparse
         import tempfile
         import queue
@@ -236,6 +77,8 @@ def speaking_Test():
         import soundfile as sf
         import numpy  # Make sure NumPy is loaded before it is used in the callback
         assert numpy  # avoid "imported but unused" message (W0611)
+        print("Og check")
+        
 
 
         def int_or_str(text):
@@ -306,7 +149,7 @@ def speaking_Test():
             parser.exit(0)
         except Exception as e:
             parser.exit(type(e).__name__ + ': ' + str(e))
-
+    global stop 
     def stop():
         return KeyboardInterrupt
       
@@ -349,8 +192,8 @@ def speaking_Test():
     print("Checkpoint 2")
     print("Checkpoint 3")
     calling()
-    Button(speaking,text = "Record my test",height = "2", width = "30", command = Og).pack()
-    Button(speaking,text = "Stop Recording ",height = "2", width = "30", command = stop).pack()
+    Button(speaking,text = "Record my test",height = "2", width = "30", command = OG2).pack()
+    
     
 
 def main_screen():
@@ -364,7 +207,7 @@ def main_screen():
   screen.title("IELTS TEST TAKER")
   Label(text = "Welcome! IELTS TEST TAKER", bg = "grey", width = "300", height = "2", font = ("Calibri", 13)).pack()
   Label(text = "", bg='#cc1a30' ).pack()
-  Button(text = "Give Listening Test", height = "2", width = "30", command ="speaking_Test" ).pack()
+  Button(text = "Give Listening Test", height = "2", width = "30", command =Listening_Test ).pack()
   Label(text = "", bg='#cc1a30' ).pack()
   Button(text = "Give Reading Test",height = "2", width = "30", command = "speaking_Test").pack()
   Label(text = "", bg='#cc1a30' ).pack()
